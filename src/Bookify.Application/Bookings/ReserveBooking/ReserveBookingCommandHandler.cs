92bebc2cd68a546eee9a3c5ff6bb06107f5dbc88
Irakli_Imesashvili <imesashviliirakli5@gmail.com>
2024-12-04 18:28:48 +0400

    Added INfrastructure and presenstation layer

--- src/Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommandHandler.cs
+++ src/Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Bookify.Application.Abstractions.Clock;
 using Bookify.Application.Abstractions.Messaging;
+using Bookify.Application.Exceptions;
 using Bookify.Domain.Abstractions;
 using Bookify.Domain.Apartments;
 using Bookify.Domain.Bookings;
@@ -42,7 +43,9 @@ internal sealed class ReserveBookingCommandHandler(
         if (await _bookingRepository.IsOverlappingAsync(apartment, duration, cancellationToken))
             return Result.Failure<Guid>(BookingErrors.Overlap);
 
-        var booking = Booking.Reserve(
+        try
+        {
+            var booking = Booking.Reserve(
                 apartment,
                 user.Id,
                 duration,
@@ -50,10 +53,15 @@ internal sealed class ReserveBookingCommandHandler(
                 _pricingService
             );
 
-        _bookingRepository.Add(booking);
+            _bookingRepository.Add(booking);
 
-        await _unitOfWork.SaveChangesAsync(cancellationToken);
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
 
-        return booking.Id;
+            return booking.Id;
+        }
+        catch(ConcurrencyException)
+        {
+            return Result.Failure<Guid>(BookingErrors.Overlap);
+        }
     }
 }
---
+++ src/Bookify.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,26 @@
+﻿using Bookify.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrastructure.Repositories;
+
+internal abstract class Repository<T>(
+        ApplicationDbContext context
+    ) where T : Entity
+{
+    protected readonly ApplicationDbContext _context = context;
+
+
+    public async Task<T?> GetByIdAsync(
+        Guid id,
+        CancellationToken cancellationToken = default)
+    {
+        return await _context
+            .Set<T>()
+            .FirstOrDefaultAsync(user => user.Id == id, cancellationToken);
+    }
+
+    public void Add(T entity)
+    {
+        _context.Add(entity);
+    }
+}
